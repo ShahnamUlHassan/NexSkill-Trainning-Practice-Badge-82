5f17563c51fe6154753c81b3477d90831ba0a82e
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-01-03 10:33:06 +0500

    Add files via upload

---
+++ Practice 6/index.js
@@ -0,0 +1,22 @@
+ alert("hi moon");
+ 
+
+ name = prompt("Enter your Name");
+ alert(name);
+ document.write(name);
+
+ studentobtainedmarks = promt("Enter your obtained marks");
+ studenttotalmarks = promt("Enter your student total marks");
+
+ res = studentobtainedmarks / studenttotalmarks * 100
+
+ alert(res);
+ document.write(res);
+
+
+
+  // data type
+  //  "  "   string
+  // number integer 111 anyone number
+  // uint 1122334433
+  // boolean used for true and false 
